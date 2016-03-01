e8d2c0236855099348a1de60733dd72aff483af0
Daniel T. Lee <danieltimlee@gmail.com>
2016-03-01 10:53:36 +0900

    .Gitignore files changed
    Merge branch 'master' of https://github.com/DanielTimLee/python_dj_helloworld
    
    # Conflicts:
    #	.gitignore

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import unittest
 
-class  NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser=webdriver.Firefox()
@@ -13,7 +13,7 @@ class  NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrive_it_later(self):
         self.browser.get('http://localhost:8000')
 
-        #self.assertIn('To-Do',self.browser.title)
+        self.assertIn('To-Do',self.browser.title)
         self.fail('Finish the Test!')
 
 if __name__=='__main__':
